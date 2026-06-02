f40075ff89259795e495c5e6179f8b271e01d8d3
Juan Lassalle <135929039+juanlassalle@users.noreply.github.com>
2026-06-02 15:59:06 -0300

    Se agrega ejercico suma de dos vectores

---
+++ Solucion_EstructuraDeDatos/02_Ejercicio/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Ejercicio
+{
+    internal class Program
+    {
+        //Se requiere un algoritmo para obtener un vector (C) de N elementos que
+        //contenga la suma de los elementos correspondientes de otros dos vectores
+        //(A y B).
+        static void Main(string[] args)
+        {
+            int nElementos;
+            Console.Write("Ingresar la cantidad de elementos de los vectores: ");
+            nElementos = int.Parse(Console.ReadLine());
+
+            int[] vectorA = new int[nElementos];
+            int[] vectorB = new int[nElementos];
+            int[] vectorC = new int[nElementos];
+
+            int i = 0;
+
+            while (i < nElementos)
+            {
+                Console.WriteLine("===Crear Vectores A y B===");
+                Console.Write($"Ingresar elemento {i}: ");
+                vectorA[i] = Convert.ToInt32(Console.ReadLine());
+                Console.Write($"Ingresar elemento {i}: ");
+                vectorB[i] = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine();
+                i++;
+            }
+
+            i = default;
+
+            while (i < nElementos)
+            {
+                vectorC[i] = vectorA[i] + vectorB[i];
+                i++;
+            }
+
+            Console.Write($"VectorC: [{string.Join(" , ", vectorC)}]");
+
+        }
+    }
+}
---
+++ Solucion_EstructuraDeDatos/02_Ejercicio/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("02_Ejercicio")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("02_Ejercicio")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("160718fb-6cef-4484-b8d2-c9f062c1131e")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
