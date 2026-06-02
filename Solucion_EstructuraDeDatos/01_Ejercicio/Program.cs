2fe3421d4348fe42b7a0d40ed52a745bcd0f25b7
Juan Lassalle <135929039+juanlassalle@users.noreply.github.com>
2026-06-02 15:21:29 -0300

    Se agrega ejercicio_1

---
+++ Solucion_EstructuraDeDatos/01_Ejercicio/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Ejercicio
+{
+    internal class Program
+    {
+        //Se requiere obtener la suma de las cantidades contenidas en un arreglo de
+        //10 elementos.
+        static void Main(string[] args)
+        {
+            int[] arreglo = { 2, 5, 6, 7, 11, 2, 3, 4, 5, 6 };
+            int suma = 0;
+
+            for (int i = 0; i <= arreglo.Length - 1; i++)
+            {
+                suma = suma + arreglo[i];
+            }
+
+            Console.Write($"El vector: [{string.Join(" , ", arreglo)}]");
+            Console.WriteLine("\n");
+            Console.Write($"La suma de los arreglos es: {suma}");
+            Console.WriteLine("\n");
+        }
+    }
+}
